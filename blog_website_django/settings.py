eb8b410815d47b832171b9b2e0293aa5656513e1
Abrar Shahid <70060400+abrarshahid@users.noreply.github.com>
2021-05-23 14:56:08 +0600

    Added Files

--- blog_website_django/settings.py
+++ blog_website_django/settings.py
@@ -76,14 +76,13 @@ WSGI_APPLICATION = 'blog_website_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    
+    'default': dj_database_url.config()
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
